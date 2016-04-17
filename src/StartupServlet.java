f6c7106edb0d46a121a57d071650f2cc9c99e8d6
Rangelov <prangelov@deloitte.com>
2016-04-16 23:49:53 -0400

    testing mailer

---
+++ src/StartupServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
+
+@WebServlet(loadOnStartup=1, urlPatterns = {"/mailer"})
+public class StartupServlet extends HttpServlet {
+	
+	
+	String user = "peter.rangelov11@gmail.com";
+	String sendgridKey = System.getenv("SENDGRID_API_KEY");
+	{
+		System.out.println("Startup servlet running");
+		
+		SendGrid sendgrid = new SendGrid(sendgridKey);
+
+	    SendGrid.Email email = new SendGrid.Email();
+	    email.addTo(user);
+	    email.setFrom(user);
+	    email.setSubject("Hello World");
+	    email.setText("My first email with SendGrid Java!");
+	    
+	    SendGrid.Response response;
+		try {
+			response = sendgrid.send(email);
+			System.out.println(response.getMessage());
+		} catch (SendGridException e) {
+			e.printStackTrace();
+		}
+	      
+	}
+
+}
