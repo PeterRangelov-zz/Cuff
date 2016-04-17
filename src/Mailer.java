d2a5f30b1eaf14b15a7b62e8f25f56b1b90ef468
Rangelov <prangelov@deloitte.com>
2016-04-17 00:19:23 -0400

    w/ Mailer

---
+++ src/Mailer.java
@@ -0,0 +1,49 @@
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
+import com.sendgrid.SendGrid.Email;
+import com.sendgrid.SendGrid.Response;
+
+public class Mailer {
+	private static final String SENDGRID_USERNAME = System.getenv("SENDGRID_USERNAME");
+	private static final String SENDGRID_API_KEY = System.getenv("SENDGRID_API_KEY");
+	private static final String SUBMISSION_RECEPIENT = System.getenv("SUBMISSION_RECEPIENT");
+	private static final String SUBMISSION_SENDER = System.getenv("SUBMISSION_SENDER");
+	
+	public void sendStartupNotification () {
+		System.out.println("Startup servlet running");
+		System.out.println("Sendgrid username: " + SENDGRID_USERNAME);
+		System.out.println("Sendgrid api key: " + SENDGRID_API_KEY);
+		
+		SendGrid sendgrid = new SendGrid(SENDGRID_API_KEY);
+
+	    Email email = new Email();
+	    email.addTo(SENDGRID_USERNAME);
+	    email.setFrom(SUBMISSION_SENDER);
+	    email.setSubject("CUFF Notification: Application Startup");
+	    email.setText("Startup Servlet triggered in environment: " + System.getenv("ENVIRONMENT"));
+	    
+	    Response response;
+		try {
+			response = sendgrid.send(email);
+			System.out.println(response.getMessage());
+		} catch (SendGridException e) {
+			e.printStackTrace();
+		}
+			
+	}
+	
+	public void emailSubmission () throws SendGridException {
+		SendGrid sendgrid = new SendGrid(SENDGRID_API_KEY);
+
+	    Email email = new Email();
+	    email.addTo(SUBMISSION_RECEPIENT);
+	    email.setFrom(SUBMISSION_SENDER);
+	    email.setSubject("CUFF Notification: Application Startup");
+	    email.setText("Startup Servlet triggered in environment: " + System.getenv("ENVIRONMENT"));
+	    
+	    Response response = sendgrid.send(email);
+		System.out.println(response.getMessage());
+	}
+	
+
+}
--- src/StartupServlet.java
+++ src/StartupServlet.java
@@ -4,30 +4,11 @@ import javax.servlet.http.HttpServlet;
 import com.sendgrid.SendGrid;
 import com.sendgrid.SendGridException;
 
-@WebServlet(loadOnStartup=1, urlPatterns = {"/mailer"})
+@WebServlet(loadOnStartup=1, urlPatterns = {"/startup"})
 public class StartupServlet extends HttpServlet {
-	
-	final String user = "peter.rangelov11@gmail.com";
-	final String sendgridKey = System.getenv("SENDGRID_API_KEY");
 	{
-		System.out.println("Startup servlet running");
-		
-		SendGrid sendgrid = new SendGrid(sendgridKey);
-
-	    SendGrid.Email email = new SendGrid.Email();
-	    email.addTo(user);
-	    email.setFrom(user);
-	    email.setSubject("Hello World");
-	    email.setText("Sending email from environment: " + System.getenv("ENVIRONMENT"));
-	    
-	    SendGrid.Response response;
-		try {
-			response = sendgrid.send(email);
-			System.out.println(response.getMessage());
-		} catch (SendGridException e) {
-			e.printStackTrace();
-		}
-	      
+		Mailer mailer = new Mailer();
+		mailer.sendStartupNotification();
 	}
 
 }
